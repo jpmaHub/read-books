3563af9b7e5b2200691e71b684b28991a3fd9a35
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2018-09-17 14:28:38 +0100

    Add all test doubles

--- spec/acceptance/read_book_spec.rb
+++ spec/acceptance/read_book_spec.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 describe 'read book' do
- let(:book_gateway) { InMemoryGateway.new }
+  let(:book_gateway) { InMemoryGateway.new }
+  #Fake
   class InMemoryGateway
     def initialize
       @book = []
@@ -7,51 +10,63 @@ describe 'read book' do
 
     def all
       @book
-    end 
+    end
 
     def save(book)
       @book.push(book)
-    end 
+    end
   end
-  
+
   context 'saving read books' do
     it 'returns nil when no books saved' do
-     save_book = SaveBook.new(book_gateway: book_gateway)
-     save_book.execute({})
-     books_read = book_gateway.all.first
-     expect(books_read).to eq(nil)
-    end 
+      save_book = SaveBook.new(book_gateway: book_gateway)
+      save_book.execute({})
+      books_read = book_gateway.all.first
+      expect(books_read).to eq(nil)
+    end
 
     it 'book gateway returns a book when a book is saved' do
       save_book = SaveBook.new(book_gateway: book_gateway)
       book_details = {
-      title: 'To Kill A Mockingbird',
-      author: 'Harper Lee'
+        title: 'To Kill A Mockingbird',
+        author: 'Harper Lee'
       }
       save_book.execute(book_details)
       books_read = book_gateway.all.first
       expect(books_read).to eq(book_details)
-     end
-  end 
+    end
+  end
 
   context 'view all read books' do
     it 'returns nil when no books read' do
-     view_books = ViewBooks.new(book_gateway: book_gateway)
-     view_books.execute
-     books_read = book_gateway.all
-     expect(books_read).to eq([])
-    end 
+      view_books = ViewBooks.new(book_gateway: book_gateway)
+      view_books.execute
+      books_read = book_gateway.all
+      expect(books_read).to eq([])
+    end
 
-    xit 'book gateway returns a book when a book is saved' do
-      save_book = SaveBook.new(book_gateway: book_gateway)
-      book_details = {
-      title: 'To Kill A Mockingbird',
-      author: 'Harper Lee'
+    it 'book gateway returns a book when a book is saved' do
+      view_book = ViewBooks.new(book_gateway: book_gateway)
+      book1 = {
+        title: 'To Kill A Mockingbird',
+        author: 'Harper Lee'
       }
-      save_book.execute(book_details)
-      books_read = book_gateway.all.first
-      expect(books_read).to eq(book_details)
-     end
-  end 
 
-end 
+      book2 = {
+        title: 'Mockingbird',
+        author: 'Lee'
+      }
+
+      save_book = SaveBook.new(book_gateway: book_gateway)
+      save_book.execute(book1)
+      save_book.execute(book2)
+
+      view_book.execute
+      books_read = book_gateway.all
+      expect(books_read).to eq([
+                                 { author: 'Harper Lee', title: 'To Kill A Mockingbird' },
+                                 { author: 'Lee', title: 'Mockingbird' }
+                               ])
+    end
+  end
+end
--- spec/unit/use_cases/view_books_spec.rb
+++ spec/unit/use_cases/view_books_spec.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 describe ViewBooks do
   let(:book_gateway) { BookGatewayStub.new }
-  
+  #Stub
   class BookGatewayStub
     def initialize
       @books = []
@@ -13,13 +15,19 @@ describe ViewBooks do
     def all
       @books
     end
-
   end
 
   it 'returns error when no books to view' do
     book_gateway.book_details([])
     view_books = ViewBooks.new(book_gateway: book_gateway)
     response = view_books.execute
-    expect(response).to eq({ successful: false, errors: :no_books_to_view })
+    expect(response).to eq(successful: false, errors: :no_books_to_view)
   end
-end 
+
+  it 'returns book details' do
+    book_gateway.book_details([{ title: 'To kill a mocking bird', author: 'Harper lee' }])
+    view_books = ViewBooks.new(book_gateway: book_gateway)
+    response = view_books.execute
+    expect(response).to eq([{ title: 'To kill a mocking bird', author: 'Harper lee' }])
+  end
+end
